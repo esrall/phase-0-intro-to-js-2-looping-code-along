2a8cdbefa407fe1c722d41af2c3153de616c811a
Martha Solomon <marthsolomon@gmail.com>
2021-12-06 22:19:46 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Samip", "Aki"]
+
+function writeCards() {
+    let i = 0; while (i < names.length) {
+        console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`);
+        i++;
+    }
+}
+
+writeCards(names);
+
+function countDown () {
+    for(let countup = 0; countup <11; countup++)
+    console.log(countup);
+}
